370f86099e9da17c6dab6ed820da4bef220da76a
Eric Stéfano <ericssm96@gmail.com>
2025-10-07 23:26:34 -0300

    Hero content and CSS setup

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,10 @@
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>Manage any content <br /> Anywhere</h1>
+        <p>Strapi is the leading open-source headless CMS. Trusted by thousands of developers and companies around the world.</p>
+      </div>
+    </div>
   )
 }
