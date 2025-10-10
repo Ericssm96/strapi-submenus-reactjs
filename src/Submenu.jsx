b992c866b2d4b1393e9a7c67c9d54307ccd698b1
Eric Stéfano <ericssm96@gmail.com>
2025-10-10 10:44:37 -0300

    Submenu hide and show behavior complete

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,13 +1,25 @@
 import sublinks from "./data";
 import { useGlobalContext } from "./Context";
+import { useRef } from "react";
 
 export const Submenu = () => {
   const {pageId, setPageId} = useGlobalContext();
   const currentPage = sublinks.find((sublink)=>sublink.pageId === pageId);
-  console.log(currentPage);
+
+  const submenuContainer = useRef(null);
+  
+  const handleOnMouseLeave = (e) => {
+    const submenu = submenuContainer.current;
+    const {clientY, clientX} = e;
+    const {bottom, right, left, top} = submenu.getBoundingClientRect();
+    
+    if(clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1){
+      setPageId(null);
+    }
+  }
 
   return (
-    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+    <div className={currentPage ? "submenu show-submenu" : "submenu"} onMouseLeave={handleOnMouseLeave} ref={submenuContainer}>
       <h5>{currentPage?.page}</h5>
 
       <div className="submenu-links" style={{display:"grid", gridTemplateColumns: currentPage?.links.length > 3 ? 'repeat(2, 1fr)' : '1fr'}}>
